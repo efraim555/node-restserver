0c1eaca8dce359923d9301ebce8c29ef59f34fa1
Efraim <regalo3xd@gmail.com>
2018-09-29 10:54:45 -0500

    Env Variable MONGO added

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Qwerty1234@ds119113.mlab.com:19113/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
